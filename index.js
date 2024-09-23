af52ebdf913d44fa287415240fe591ac2f36a6d2
mahade <mahade.adib45@gmail.com>
2024-09-23 16:04:01 +0600

    edited backend

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// comment line
- mahadi
-//Hello Mahadi
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+// middlewires
+app.use(cors())
+app.use(express.json())
+
+//db
+mongoose.connect(process.env.URI).then(() => console.log("Connected to DB!"))
 
 
+const companyCollection = client.db("expenseMaster").collection("Company");
+const userCollection = client.db("expenseMaster").collection("User");
 
-//test 2 mahadi
-=======
 
+app.listen(port, () => {
+    console.log("running at", port);
+})
